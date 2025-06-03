b10060b4dccdcf80d6d550e5e60c3c24550552de
vanbui262004 <vanbui0966467356@gmail.com>
2025-06-03 17:40:32 +0700

    X

--- src/map/TwoSum.java
+++ src/map/TwoSum.java
@@ -1,10 +1,18 @@
 package map;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
+
+/*
+ * Author: BUI QUOC VAN
+ * Ex: TwoSum
+ * Date: 3/6/2025
+ */
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>(); // value -> index
+        Map<Integer, Integer> map = new HashMap<>(); 
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
             if (map.containsKey(complement)) {
@@ -12,13 +20,12 @@ public class TwoSum {
             }
             map.put(nums[i], i);
         }
-        return new int[] {}; // Nếu không tìm thấy
+        return new int[] {};
     }
-
     // Demo
     public static void main(String[] args) {
         TwoSum ts = new TwoSum();
         int[] result = ts.twoSum(new int[] {2, 7, 11, 15}, 9);
-        System.out.println("Output: [" + result[0] + ", " + result[1] + "]");
+        System.out.println("Output: " + Arrays.toString(result));
     }
 }
