e53d0fd47520d10ec1d7386165b08f3ec0903df0
vanbui262004 <vanbui0966467356@gmail.com>
2025-06-12 14:15:14 +0700

    PalindromeChecker

---
+++ src/string/PalindromeChecker.java
@@ -0,0 +1,17 @@
+package string;
+
+public class PalindromeChecker  {
+	 public static boolean isPalindrome(String str) {
+	        if (str == null) return false;
+	        str = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+	        int left = 0, right = str.length() - 1;
+	        while (left < right) {
+	            if (str.charAt(left) != str.charAt(right)) {
+	                return false;
+	            }
+	            left++;
+	            right--;
+	        }
+	        return true;
+	    }
+}
